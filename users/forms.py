a485b163ae09280e1d4093ea03ecf272b7adfa7a
okusjid <l200927@lhr.nu.edu.pk>
2024-09-03 21:11:06 +0500

    [updated] structure

--- users/forms.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.forms import AuthenticationForm
-from django import forms
-
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'bg-gradient-to-l hover:bg-gradient-to-r'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
