790f10914fb6a805a4c52078ad2a9e9c261702af
yousaf-360 <yousafch360@gmail.com>
2024-09-10 15:49:02 +0500

    added validation on creation

--- SRC/core/forms.py
+++ SRC/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Doctor
+from .models import Doctor, Patient
 from users.models import User
 
 class DoctorForm(forms.ModelForm):
@@ -7,11 +7,24 @@ class DoctorForm(forms.ModelForm):
     email = forms.EmailField()
     phone_number = forms.CharField(max_length=15)
     password = forms.CharField(widget=forms.PasswordInput(render_value=False), required=False)
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if self.instance.pk:  
+            if User.objects.filter(username=username).exclude(id=self.instance.user.id).exists():
+                raise forms.ValidationError("This username is already in use.")
+        else:  
+            if User.objects.filter(username=username).exists():
+                raise forms.ValidationError("This username is already in use.")
+        return username
 
     def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists() and not self.instance.user.email == email:
-            raise forms.ValidationError("This email is already in use.")
+        email = self.cleaned_data.get('email')
+        if self.instance.pk: 
+            if User.objects.filter(email=email).exclude(id=self.instance.user.id).exists():
+                raise forms.ValidationError("This email is already in use.")
+        else:  
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("This email is already in use.")
         return email
     
     class Meta:
@@ -27,3 +40,45 @@ class DoctorForm(forms.ModelForm):
             self.fields['phone_number'].initial = instance.user.phone_number
             self.fields['password'].required = False
         
+class PatientForm(forms.ModelForm):
+    username = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    phone_number = forms.CharField(max_length=15)
+    password = forms.CharField(widget=forms.PasswordInput(render_value=False), required=False)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if self.instance.pk:  
+            if User.objects.filter(username=username).exclude(id=self.instance.user.id).exists():
+                raise forms.ValidationError("This username is already in use.")
+        else:  
+            if User.objects.filter(username=username).exists():
+                raise forms.ValidationError("This username is already in use.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if self.instance.pk: 
+            if User.objects.filter(email=email).exclude(id=self.instance.user.id).exists():
+                raise forms.ValidationError("This email is already in use.")
+        else:  
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("This email is already in use.")
+        return email
+
+    class Meta:
+        model = Patient
+        fields = ['date_of_birth', 'gender']
+
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance')
+        super().__init__(*args, **kwargs)
+        
+        if instance and instance.user:
+            self.fields['username'].initial = instance.user.username
+            self.fields['email'].initial = instance.user.email
+            self.fields['phone_number'].initial = instance.user.phone_number
+            self.fields['password'].required = False
+
+
+
--- SRC/core/views/patient.py
+++ SRC/core/views/patient.py
@@ -7,8 +7,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.urls import reverse_lazy
 from django.views import View
-
-from django.views.generic.edit import FormView,UpdateView,DeleteView
+from core.forms import PatientForm
+from django.views.generic.edit import FormView,UpdateView,DeleteView,CreateView
+from django.core.exceptions import ValidationError
 
 
 User = get_user_model()
@@ -32,84 +33,58 @@ class PatientDetailView(LoginRequiredMixin, DetailView):
     
     login_url = '/login/'
 
-class PatientCreateView(LoginRequiredMixin, View):
+class PatientCreateView(LoginRequiredMixin, CreateView):
+    model = Patient
+    form_class = PatientForm
     template_name = 'core/patients/patient_form.html'
     success_url = reverse_lazy('patient_list')
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
-
-    def post(self, request, *args, **kwargs):
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        phone_number = request.POST.get('phone_number')
-        date_of_birth = request.POST.get('date_of_birth')
-        gender = request.POST.get('gender')
-        password = request.POST.get('password')
-
-        user = User.objects.create_user(
-            username=username,
-            email=email,
-            password=password,
-            phone_number=phone_number
-        )
-
-        Patient.objects.create(
-            user=user,
-            date_of_birth=date_of_birth,
-            gender=gender
-        )
-
-        messages.success(request, 'Patient created successfully!')
-        return redirect(self.success_url)
-
+    def form_valid(self, form):
+        try:
+            user = User.objects.create(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                phone_number=form.cleaned_data['phone_number']
+            )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            form.instance.user = user
+            return super().form_valid(form)
+        except Exception as e:
+            form.add_error(None, "An error occurred while creating the user. Please try again.")
+            return self.form_invalid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create Patient'
+        context['button_name'] = 'Create'
+        return context
 
 
 class PatientUpdateView(LoginRequiredMixin, UpdateView):
     model = Patient
-    template_name = 'core/patients/update_patient_form.html'
-    fields = ['date_of_birth', 'gender']
-    
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        patient = self.get_object()
-        kwargs['initial'] = {
-            'username': patient.user.username,
-            'email': patient.user.email,
-            'phone_number': patient.user.phone_number,
-            'date_of_birth': patient.date_of_birth,
-            'gender': patient.gender
-        }
-        return kwargs
+    form_class = PatientForm
+    template_name = 'core/patients/patient_form.html'
+    success_url = reverse_lazy('patient_list')
 
     def form_valid(self, form):
-        patient = self.get_object()
-        user = patient.user
-
-        user.username = self.request.POST.get('username')
-        user.email = self.request.POST.get('email')
-        user.phone_number = self.request.POST.get('phone_number')
+        user = form.instance.user
+        user.username = form.cleaned_data['username']
+        user.email = form.cleaned_data['email']
+        user.phone_number = form.cleaned_data['phone_number']
+        if form.cleaned_data['password']:
+            user.set_password(form.cleaned_data['password'])
         user.save()
-
-        patient.date_of_birth = self.request.POST.get('date_of_birth')
-        patient.gender = self.request.POST.get('gender')
-        patient.save()
-
-        messages.success(self.request, 'Patient updated successfully!')
         return super().form_valid(form)
     
-    def get_success_url(self):
-        return reverse_lazy('patient_list')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Update Patient'
+        context['button_name'] = 'Update'
+        return context
 
 
 class PatientDeleteView(LoginRequiredMixin, DeleteView):
     model = Patient
     template_name = 'core/patients/patient_confirm_delete.html'
     success_url = reverse_lazy('patient_list')
-
-    def delete(self, request, *args, **kwargs):
-        patient = self.get_object()
-        patient.user.delete()
-        
-        messages.success(self.request, 'Patient deleted successfully!')
-        return super().delete(request, *args, **kwargs)
